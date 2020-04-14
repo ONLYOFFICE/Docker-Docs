86d82c8bab570b504789248534e04e7a223482b7
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2020-04-14 16:18:15 +0300

    Refactore setup NODE_CONFIG

--- start-helper.sh
+++ start-helper.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-DB_NAME=${DB_NAME:-localhost}
-DB_NAME=${DB_NAME:-onlyoffice}
-DB_USER=${DB_USER:-onlyoffice}
-DB_PWD=${DB_PWD:-onlyoffice}
-DB_TYPE=${DB_TYPE:-postgres}
-REDIST_SERVER_HOST=${REDIST_SERVER_HOST:-localhost}
-AMQP_URI=${AMQP_URI:-'amqp://guest:guest@localhost'}
-
-export NODE_CONFIG='{"services":{"CoAuthoring":{"sql":{"dbHost": "'$DB_HOST'","dbName":"'$DB_NAME'","dbUser":"'$DB_USER'","dbPass":"'$DB_PWD'"},"redis":{"host": "'$REDIST_SERVER_HOST'","port":"6379"}}},"rabbitmq":{"url": "'$AMQP_URI'"}}'
-
-echo $@
+export NODE_CONFIG='{
+  "services": {
+    "CoAuthoring": {
+      "sql": {
+        "dbHost": "'${DB_HOST:-localhost}'",
+        "dbPort": '${DB_PORT:-5432}',
+        "dbName": "'${DB_NAME:-onlyoffice}'",
+        "dbUser": "'${DB_USER:-onlyoffice}'",
+        "dbPass": "'${DB_PWD:-onlyoffice}'"
+      },
+      "redis": {
+        "host": "'${REDIST_SERVER_HOST:-localhost}'",
+        "port": '${REDIST_SERVER_PORT:-6379}'
+      }
+    }
+  },
+  "rabbitmq": {
+    "url": "'${AMQP_URI:-"amqp://guest:guest@localhost"}'"
+  },
+}'
 
 exec "$@"
