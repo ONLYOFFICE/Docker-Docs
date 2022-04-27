c6ee02aeb2270bc601ed88cefd3b9bd56c4367f7
Dmitry Kireev <octavarium78@gmail.com>
2022-04-27 10:13:45 +0300

    Add activemq support (#48)
    * Add activemq support
    
    * Fix indent
    
    * Fix readme
    
    * Change variable names
    
    * Сhange definition of variables
    
    * Сhange definition of variables to case
    
    * Change default variable assignment
    
    * Add variable for activemq transport
    
    * Сhange definition of activemq tramsport variable to case
    
    * Fix variable assignment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,6 +13,16 @@ if [[ -n ${LOG_PATTERN} ]]; then
   sed "s/\(\"pattern\"\:\).*/\1 \"$LOG_PATTERN\"/" -i /etc/$COMPANY_NAME/documentserver/log4js/production.json
 fi
 
+ACTIVEMQ_TRANSPORT=""
+case $AMQP_PROTO in
+  amqps | amqp+ssl)
+    ACTIVEMQ_TRANSPORT="tls"
+    ;;
+  *)
+    ACTIVEMQ_TRANSPORT="tcp"
+    ;;
+esac
+
 export NODE_CONFIG='{
   "statsd": {
     "useMetrics": '${METRICS_ENABLED:-false}',
@@ -63,8 +73,20 @@ export NODE_CONFIG='{
       }
     }
   },
+  "queue": {
+    "type": "'${AMQP_TYPE:=rabbitmq}'"
+  },
+  "activemq": {
+    "connectOptions": {
+      "port": "'${AMQP_PORT:=5672}'",
+      "host": "'${AMQP_HOST:=localhost}'",
+      "username": "'${AMQP_USER:=guest}'",
+      "password": "'${AMQP_PWD:=guest}'",
+      "transport": "'${ACTIVEMQ_TRANSPORT}'"
+    }
+  },
   "rabbitmq": {
-    "url": "'${AMQP_URI:-${AMQP_PROTO:-amqp}://${AMQP_USER:-guest}:${AMQP_PWD:-guest}@${AMQP_HOST:-localhost}}'"
+    "url": "'${AMQP_URI:-${AMQP_PROTO:-amqp}://${AMQP_USER}:${AMQP_PWD}@${AMQP_HOST}:${AMQP_PORT}${AMQP_VHOST:-/}}'"
   },
   "wopi": {
     "enable": "'${WOPI_ENABLED:-false}'"
