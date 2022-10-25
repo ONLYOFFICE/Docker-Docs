a75b0c508336698e92bd5d3bc1026c359a31f839
VyacheslavSemin <sla1733@yandex.ru>
2022-10-25 15:47:17 +0000

    Fix the variable name

--- proxy-docker-entrypoint.sh
+++ proxy-docker-entrypoint.sh
@@ -12,8 +12,8 @@ fi
 envsubst < /tmp/proxy_nginx/includes/http-upstream.conf > /tmp/http-upstream.conf
 envsubst < /etc/nginx/includes/ds-common.conf | tee /tmp/proxy_nginx/includes/ds-common.conf > /dev/null
 sed "s,\(set \+\$secure_link_secret\).*,\1 "${SECURE_LINK_SECRET:-verysecretstring}";," -i /tmp/proxy_nginx/conf.d/ds.conf
-if [[ -n "$IP_ACCESS_INFO" ]]; then
-  declare -a IP_ALL=($IP_ACCESS_INFO)
+if [[ -n "$INFO_ALLOWED_IP" ]]; then
+  declare -a IP_ALL=($INFO_ALLOWED_IP)
   for ip in "${IP_ALL[@]}"; do
     sed -i '/(info)/a\  allow '$ip'\;' /tmp/proxy_nginx/includes/ds-docservice.conf
   done
