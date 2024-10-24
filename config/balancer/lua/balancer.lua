047c68ccaf923b0c62a3125819f1093154c3a92f
danilapog <danil.titarenko@onlyoffice.com>
2024-10-24 11:14:23 +0300

    Do not get ep by versions

--- config/balancer/lua/balancer.lua
+++ config/balancer/lua/balancer.lua
@@ -35,53 +35,12 @@ function _M.balance()
    local decoded_data = cjson.decode(data)
    local matching_addresses = {}
    
-   if not api_key and not ver then
-     for _, entry in ipairs(decoded_data) do
-           table.insert(matching_addresses, entry.address .. ":" .. entry.port)
-     end
-   end
-
-   if api_key then
-     for _, entry in ipairs(decoded_data) do
-           table.insert(matching_addresses, entry.address .. ":" .. entry.port)
-     end
-   else
-     if ver then
        -- Iterate through the decoded table
-       for _, entry in ipairs(decoded_data) do
-         if entry.ver == ver then
+   for _, entry in ipairs(decoded_data) do
              table.insert(matching_addresses, entry.address .. ":" .. entry.port)
-         end
-       end
-     end
    end
 
-   if ver and next(matching_addresses) == nil then
-     print(string.format("WARN: Can't find endpoint in live table. VER: %s", ver))
-     local reserved_data
-     repeat
-       reserved_data = configuration.get_reserved_data()
-       print(string.format("RESERVED_DATA:%s", reserved_data))
-       local decoded_reserved_table = cjson.decode(reserved_data)
-       print(tostring(decoded_reserved_table))
-       local address = decoded_reserved_table[1].address
-       print(cjson.encode(address))
-       if address == "none" then
-         ngx.sleep(1)
-         print("No active shards found, waiting...")
-       end
-     until address ~= "none"
-     local reserved_decoded_data = cjson.decode(reserved_data)
-     local reserved_addresses = {}
-     for _, entry in ipairs(reserved_decoded_data) do
-       if entry.ver == ver then
-           table.insert(reserved_addresses, entry.address .. ":" .. entry.port)
-       end
-     end
-     random_endpoint = tostring((reserved_addresses[math.random(1, #reserved_addresses)]))
-   else
-     random_endpoint = tostring((matching_addresses[math.random(1, #matching_addresses)]))
-   end
+   random_endpoint = tostring((matching_addresses[math.random(1, #matching_addresses)]))
 
    if api_key then
      return random_endpoint
